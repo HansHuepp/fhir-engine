e64831b57ff6daf700c0bd43a69af5dbc0cc68c8
Asaad <email.asaad@mutareb.com>
2024-10-25 10:18:42 +0000

    added dummy answers

--- logic_check.py
+++ logic_check.py
@@ -2,15 +2,16 @@ import json
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
-app = FastAPI(title="FHIR Questionnaire API")
+# Read the JSON file
+with open('answer.json', 'r') as file:
+    answers = json.load(file)
+
+app = FastAPI(title="FHIR Questionnaire Answers API")
+
 
 @app.get("item/{item_id}")
-async def get_item(link_id: str) -> dict:
-
-
-@app.post("istem/{item_id}")
-
-
+async def get_item(link_id: str) -> str:
+    answers.question("item_id")
 
 @app.get("item/{item_id}/check")
 async def get_item(operator: str, answerString: str):
